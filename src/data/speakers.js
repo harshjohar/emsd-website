71cc1a50638949fe0c15f24e9e8c336393d8e9d4
Harshpreet Singh Johar <hpsj3112@gmail.com>
2024-03-17 17:08:26 +0530

    Update speaker designations in speakers.js

--- src/data/speakers.js
+++ src/data/speakers.js
@@ -2,7 +2,15 @@ export const SPEAKERS = [
     {
         name: "Dr. Reinhard Miller",
         image: "/assets/speakers/reinhard.png",
-        designation: "TU Darmstadt, Germany",
+        designation:
+            "[ Editor, Advances in Colloid and Interface Science (Elsevier, I.F.: 15.6)] TU Darmstadt, Germany",
+        // link: "https://research.manchester.ac.uk/en/persons/a.song",
+    },
+    {
+        name: "Professor Chenzhong Li",
+        image: "/assets/speakers/Professor Chenzhong Li.jpg",
+        designation:
+            "Editor-in-Chief, Biosensors and Bioelectronics (Elsevier, I.F.: 12.6), The Chinese University of Hong Kong, Shenzhen, China",
         // link: "https://research.manchester.ac.uk/en/persons/a.song",
     },
     {
@@ -14,7 +22,8 @@ export const SPEAKERS = [
     {
         name: "Professor David Amabilino",
         image: "/assets/speakers/amabilino.jpg",
-        designation: "FRSC, Director, Institute of Materials Science of Barcelona, Spain",
+        designation:
+            "FRSC, Director, Institute of Materials Science of Barcelona, Spain",
         // link: "https://didattica.uniroma2.it/docenti/curriculum/3746-Gian-Carlo-Cardarilli/",
     },
     {
@@ -26,7 +35,8 @@ export const SPEAKERS = [
     {
         name: "Professor Aziz Amine",
         image: "/assets/speakers/aziz.jpeg",
-        designation: "Hassan II University of Casablanca, Morocco",
+        designation:
+            "[Editor, Biosensors and Bioelectronics (Elsevier, I.F.: 12.6)] Hassan II University of Casablanca, Morocco",
         // link: "https://didattica.uniroma2.it/docenti/curriculum/3746-Gian-Carlo-Cardarilli/",
     },
     {
@@ -69,6 +79,12 @@ export const SPEAKERS = [
         name: "Professor Nikhil Bhalla",
         designation: "Ulster University, UK",
         image: "/assets/speakers/nikhil.png",
+        // link: "",
+    },
+    {
+        name: "Dr Gurwinder Singh",
+        designation: "The University of Newcastle, Australia",
+        image: "/assets/speakers/Dr Gurwinder Singh.png",
         // link: "",
     },
 ];
